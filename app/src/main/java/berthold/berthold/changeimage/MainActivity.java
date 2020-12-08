9c034883bb9941327c3cb9cf9b46101d2cc6c408
JannikAlaska <jannik@berthold-ilsenburg.de>
2020-12-08 18:08:13 +0100

    Initial commit

---
+++ app/src/main/java/berthold/berthold/changeimage/MainActivity.java
@@ -0,0 +1,24 @@
+package berthold.berthold.changeimage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void switchImage(View view) {
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.brockenlauf);
+    }
+
+}
